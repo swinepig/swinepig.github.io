1a2da1162bb3145a7a233c213c2da1b8fe6f5e9e
swinepig <409989258@qq.com>
2016-09-05 11:53:25 +0800

    Site updated: 2016-09-05 11:53:24

--- js/totop.js
+++ js/totop.js
@@ -23,4 +23,48 @@ $("#rocket").click(function() {
     $(this).removeClass("about_item_hover");
   });
 
+$("#nav-menu a").click(function (e) {
+
+    console.log($(this).index());
+		// what tab was pressed
+		var whatTab = $(this).index();
+
+		// Work out how far the slider needs to go
+		var howFar = 85 * whatTab;
+
+
+		/* Add the ripple */
+
+		// Remove olds ones
+		$(".ripple").remove();
+
+		// Setup
+		var posX = $(this).offset().left,
+	  posY = $(this).offset().top,
+	  buttonWidth = $(this).width(),
+	  buttonHeight = $(this).height();
+
+		// Add the element
+		$(this).prepend("<span class='ripple'></span>");
+
+		// Make it round!
+		if (buttonWidth >= buttonHeight) {
+			buttonHeight = buttonWidth;
+		} else {
+			buttonWidth = buttonHeight;
+		}
+
+		// Get the center of the element
+		var x = e.pageX - posX - buttonWidth / 2;
+		var y = e.pageY - posY - buttonHeight / 2;
+
+		// Add the ripples CSS and start the animation
+		$(".ripple").css({
+			width: buttonWidth,
+			height: buttonHeight,
+			top: y + 'px',
+			left: x + 'px'
+		}).addClass("rippleEffect");
+	});
+
 })(jQuery);
