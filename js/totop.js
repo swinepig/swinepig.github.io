49e5c52d3f524bf15dbbc579675027552f49235e
swinepig <409989258@qq.com>
2016-09-06 15:52:40 +0800

    Site updated: 2016-09-06 15:52:40

--- js/totop.js
+++ js/totop.js
@@ -64,5 +64,12 @@ $("#nav-menu a").click(function (e) {
 			left: x + 'px'
 		}).addClass("rippleEffect");
 	});
+	$('.event_year label').click(function(){
+		$('.event_year>li').removeClass('current');
+		$(this).parent('li').addClass('current');
+		var year = $(this).attr('for');
+		$('#'+year).parent().prevAll('div').slideUp(800);
+		$('#'+year).parent().slideDown(800).nextAll('div').slideDown(800);
+	});
 
 })(jQuery);
